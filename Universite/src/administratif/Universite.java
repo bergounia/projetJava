057b727a761d409d676bc6f0bd70c568bb30646d
aymeric <aymericbride@yahoo.fr>
2013-09-25 16:24:39 +0200

    creation des attributs dans Universite

--- Universite/src/administratif/Universite.java
+++ Universite/src/administratif/Universite.java
@@ -1,5 +1,13 @@
 package administratif;
 
+import java.util.ArrayList;
+
 public class Universite {
+	
+	private String nom;
+	private ArrayList<UFR> ufr;
+	private ArrayList<Laboratoire> labo;
+	private ArrayList<Departement> depart;
+	
 	
 }
