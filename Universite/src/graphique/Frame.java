f393efd07596cec29ede355909193acc91fcfa8b
kamin002 <benesis002@outlook.com>
2013-09-25 17:41:01 +0200

    Swing
    début de Réalisation

---
+++ Universite/src/graphique/Frame.java
@@ -0,0 +1,32 @@
+package graphique;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Frame extends JFrame{
+	
+	private Panel pan;
+	
+	public Frame(String titre){
+		
+		super(titre);
+		this.pan=new Panel();
+		
+		this.pack();
+	}
+
+
+	public static void main(String [] args) 
+    {
+    	SwingUtilities.invokeLater(new Runnable() //Processus qui va gérer tout ce qui est lié à l'interface pour éviter les conflits avec le proccessus Main
+    	{
+    		public void run() 
+    		{
+    			Frame f = new Frame("universite");
+        		f.setSize(300,200);
+        		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        		f.setVisible(true);
+    		}
+    	});
+    }      
+}
---
+++ Universite/src/graphique/Panel.java
@@ -0,0 +1,30 @@
+package graphique;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class Panel extends JPanel{
+
+	public Panel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Panel(boolean arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Panel(LayoutManager arg0, boolean arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Panel(LayoutManager arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
