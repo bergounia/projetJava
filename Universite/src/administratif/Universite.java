8ed29bd07fec27cc9a29be524f565d1ea2982316
aymeric <aymericbride@yahoo.fr>
2013-09-25 16:38:45 +0200

    Fin de la creation de Universite

--- Universite/src/administratif/Universite.java
+++ Universite/src/administratif/Universite.java
@@ -9,5 +9,36 @@ public class Universite {
 	private ArrayList<Laboratoire> labo;
 	private ArrayList<Departement> depart;
 	
+	public Universite()
+	{
+		this.nom= "";
+		this.ufr= new ArrayList<UFR>();
+		this.labo= new ArrayList<Laboratoire>();
+		this.depart= new ArrayList<Departement>();
+	}
 	
+	public Universite(String nom, ArrayList<UFR> u, ArrayList<Laboratoire> l, ArrayList<Departement> d)
+	{
+		this.nom= nom;
+		this.ufr= u;
+		this.depart= d;
+	}
+	
+	public Object clone() throws CloneNotSupportedException
+	{
+			return super.clone();
+	}
+	
+	@Override
+	public String toString()
+	{
+		String res= new String();
+		
+		res+="L'universite: "+ this.nom + "contient "+ this.ufr+ 
+				" mais aussi "+ this.labo+
+				" et enfin "+ this.depart;
+				
+		return res;
+		
+	}
 }
