c1c59d5973245d1e01a48e88c093cad2dc880ec7
aymeric <aymericbride@yahoo.fr>
2013-09-26 09:30:15 +0200

    Correction des relations

--- Universite/src/administratif/Laboratoire.java
+++ Universite/src/administratif/Laboratoire.java
@@ -1,37 +1,29 @@
 package administratif;
 
 import java.util.ArrayList;
-
 import personnel.Chercheur;
-
-
 
 public class Laboratoire implements Chercheur {
 
 	private String nom;
 	private ArrayList<Chercheur> p;
-	private ArrayList<Departement> depart;
 
 	public Laboratoire()
 	{
 		this.nom="";
 		this.p= new ArrayList<Chercheur>();
-		this.depart= new ArrayList<Departement>();
 	}
 
-	public Laboratoire(String n, ArrayList<Chercheur> liste, ArrayList<Departement> d)
+	public Laboratoire(String n, ArrayList<Chercheur> liste)
 	{
 		this.nom= n;
-		this.p= liste;
-		this.depart= d;
-		
+		this.p= liste;		
 	}
 
 	public Laboratoire(Laboratoire d)
 	{
 		this.nom= d.nom;
 		this.p= d.p;
-		this.depart= d.depart;
 	}
 
 	public Object clone() throws CloneNotSupportedException
--- Universite/src/administratif/UFR.java
+++ Universite/src/administratif/UFR.java
@@ -8,20 +8,24 @@ public class UFR {
 	
 	private String nom;
 	private ArrayList<Batiment> b;
+	private ArrayList<Departement> d;
 
-	public UFR(String nom, ArrayList<Batiment> bat) {
+	public UFR(String nom, ArrayList<Batiment> bat, ArrayList<Departement> depart) {
 		this.nom = nom;
 		this.b= bat;
+		this.d= depart;
 	}
 	
 	public UFR(UFR u) {
 		this.nom = u.nom;
 		this.b= u.b;
+		this.d= u.d;
 	}
 	
 	public UFR() {
 		this.nom = "noname";
 		this.b= new ArrayList<Batiment>();
+		this.d= new ArrayList<Departement>();
 	}
 	
 	public Object clone() throws CloneNotSupportedException
